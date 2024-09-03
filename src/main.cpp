18331c20b19d0692acfd525c83957d3b404ea635
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2024-09-03 11:52:18 +1000

    Update main.cpp
    Skeleton code

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,10 @@
 
 #define LED_PIN 14
 
+int change_led(uint8_t led_number, uint8_t color) {
+    
+}
+
 int main()
 {
     stdio_init_all();
@@ -15,24 +19,61 @@ int main()
     // Initialise PIO0 to control the LED chain
     uint pio_program_offset = pio_add_program(pio0, &ws2812_program);
     ws2812_program_init(pio0, 0, pio_program_offset, LED_PIN, 800000, false);
-    uint32_t led_data [1];
+    static uint32_t led_data [1];
 
     for (;;) {
         // Test the log system
-        log(LogLevel::INFORMATION, "Hello world");
+        // log(LogLevel::INFORMATION, "Hello world");
 
-        // Turn on the first LED to be a certain colour
-        uint8_t red = 0;
-        uint8_t green = 0;
-        uint8_t blue = 255;
-        led_data[0] = (red << 24) | (green << 16) | (blue << 8);
-        pio_sm_put_blocking(pio0, 0, led_data[0]);
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+        
+        for(int i = 0; i < 12; i++) {
+            if(i % 2 == 0) {
+                red = 255;
+                green = 0;
+                blue = 0;
+                led_data[0] = (red << 24) | (green << 16) | (blue << 8);
+                pio_sm_put_blocking(pio0, 0, led_data[0]);
+            }
+            else{
+                red = 0;
+                green = 0;
+                blue = 255;
+                led_data[0] = (red << 24) | (green << 16) | (blue << 8);
+                pio_sm_put_blocking(pio0, 0, led_data[0]);
+            }
+        }
         sleep_ms(500);
 
-        // Set the first LED off 
-        led_data[0] = 0;
-        pio_sm_put_blocking(pio0, 0, led_data[0]);
-        sleep_ms(500);
+
+        // // Turn on the first LED to be a certain colour
+        // uint8_t red = 255;
+        // uint8_t green = 0;
+        // uint8_t blue = 0;
+        // led_data[0] = (red << 24) | (green << 16) | (blue << 8);
+        // pio_sm_put_blocking(pio0, 0, led_data[0]);
+        // //sleep_ms(500);
+
+        // red = 0;
+        // green = 255;
+        // blue = 0;
+        // led_data[1] = (red << 24) | (green << 16) | (blue << 8);
+        // pio_sm_put_blocking(pio0, 0, led_data[1]);
+
+        // red = 0;
+        // green = 0;
+        // blue = 255;
+        // led_data[2] = (red << 24) | (green << 16) | (blue << 8);
+        // pio_sm_put_blocking(pio0, 0, led_data[2]);
+
+        // sleep_ms(500); //LEDs reset if it pauses for more than 200us
+
+        // // Set the first LED off 
+        // led_data[0] = 0;
+        // pio_sm_put_blocking(pio0, 0, led_data[0]);
+        // sleep_ms(500);
     }
 
     return 0;
