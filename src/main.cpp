ca37c2318ae67a3b494dfb50866d18ff0e7e3774
johnedmer-ortiz <140313217+johnedmer-ortiz@users.noreply.github.com>
2024-10-01 12:04:49 +1000

    Add q15 and shifting functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,35 @@
 #include "boards.h"
 #include "microphone.h"
 #include <stdio.h>
+#include "arm_math.h"
 
 #define MIC_PIN 26
 #define N_SAMPLES 1024
+#define SHIFT_AMOUNT 3
 
 int main() {
     stdio_init_all();
     configure_adc_continuous(MIC_PIN);
+    // uint16_t sample_buf[N_SAMPLES];
+    // mic_read(sample_buf, N_SAMPLES);
+
+    int fft_dir = 0; // 0 for forward transform, 1 for inverse transform
+    arm_rfft_instance_q15 fft_instance;
+    arm_rfft_init_q15(&fft_instance, N_SAMPLES, fft_dir, 1);
     uint16_t sample_buf[N_SAMPLES];
     mic_read(sample_buf, N_SAMPLES);
+    
+    int32_t sum;
+    for(int i = 0; i < N_SAMPLES; i++){
+        sum = sum + sample_buf[i];
+    }
+    int32_t dc_bias = sum / N_SAMPLES;
+    printf("%d\n", dc_bias);    
+
+    int16_t processed_buf[N_SAMPLES];
+    printf("```````PROCESSED BUFFER:```````\n");
+    for (int i = 0; i < N_SAMPLES; i++) {
+        processed_buf[i] = (int16_t)((sample_buf[i] - dc_bias) << SHIFT_AMOUNT);
+        printf("%d\n", processed_buf[i]);    
+    }  
 }
